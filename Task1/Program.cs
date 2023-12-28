d4ba545bc4d93c999baaa0d1d131a82c91f35bed
Andrey Proskurin <>
2023-12-28 19:28:51 +0300

    Добавил решение

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+int M = 1; // Начало интервала
+        int N = 10; // Конец интервала
+
+        PrintNaturalNumbers(M, N);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNaturalNumbers(int currentNumber, int end)
+    {
+        if (currentNumber <= end)
+        {
+            Console.WriteLine(currentNumber);
+            PrintNaturalNumbers(currentNumber + 1, end);
+        }
+    }
